68bab8199bff1f63332969a15355ef4ebaef9b7b
jdlrobson <jdlrobson@gmail.com>
2016-09-20 14:31:16 -0700

    Move SimpleSkins to extension loading
    This will avoid issues with old MediaWiki installs trying to use
    SimpleSkins.
    
    Fixes: #1

--- SimpleSkins.php
+++ SimpleSkins.php
@@ -1,48 +1,7 @@
 <?php
 
-/**
- * Extension Gather
- *
- * @file
- * @ingroup Extensions
- * @author Jon Robson
- * @licence GNU General Public Licence 2.0 or later
- */
-
-// Needs to be called within MediaWiki; not standalone
-if ( !defined( 'MEDIAWIKI' ) ) {
-	echo "This is a MediaWiki extension and cannot run standalone.\n";
-	die( -1 );
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'SimpleSkins' );
+} else {
+	die( 'This version of the SimpleSkins extension requires MediaWiki 1.25+' );
 }
-
-// Extension credits that will show up on Special:Version
-$wgExtensionCredits['other'][] = array(
-	'path' => __FILE__,
-	'name' => 'SimpleSkins',
-	'author' => array( 'Jon Robson' ),
-	'url' => 'https://www.mediawiki.org/wiki/Extension:SimpleSkins',
-	'license-name' => 'GPL-2.0+',
-);
-
-// autoload extension classes
-$autoloadClasses = array (
-	'SimpleSkinsHooks' => 'SimpleSkins.hooks',
-	'SimpleSkin' => 'SimpleSkin',
-	'SimpleSkinTemplate' => 'SimpleSkinTemplate',
-);
-
-foreach ( $autoloadClasses as $className => $classFilename ) {
-	$wgAutoloadClasses[$className] = __DIR__ . "/includes/$classFilename.php";
-}
-
-$wgSFResourceBoilerplate = array(
-	'targets' => array( 'desktop', 'mobile' ),
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'SimpleSkins',
-);
-
-$wgSimpleSkinDefault = 'Dali';
-
-$wgHooks['OutputPageParserOutput'][] = 'SimpleSkinsHooks::onOutputPageParserOutput';
-$wgHooks['SetupAfterCache'][] = 'SimpleSkinsHooks::onSetupAfterCache';
-$wgHooks['ResourceLoaderRegisterModules'][] = 'SimpleSkinsHooks::onResourceLoaderRegisterModules';
